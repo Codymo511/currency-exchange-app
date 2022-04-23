f552616765785a6c8e908704f1f550d29da2c2df
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2022-04-23 11:48:10 -0400

    Update CurrencyConverter.js

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,34 +1,63 @@
 import React from 'react';
 import currencies from './utils/currencies';
-//need to first pull currencies to select which one you want to convert ( so two boxes, one for each currency)
-//Next I need to be able to change the amount of the currency, open box with number to type in
+import {Status, json } from './utils/fetchUtils';
+
+
 // next it needs to display the amount of the converted currency (ie 1k aud to usd is 736)
 class CurrencyConverter extends React.Component  {
   constructor() {
     super();
     this.state = {
       base: 'USD',
+      baseValue:0,
       rates: '',
       loading: true,
-      currencyAmount:'',
-      secondCurrency:'AUD'
+      amount:'',
+      secondCurrency:'AUD',
+      secondValue:0,
     }
   }
   changeBase = (event) => {
     this.setState({ base: event.target.value });
+    this.getRatesData(event.target.value);
   }
   
   changeCurrencyAmount = (event)=>{
-    this.setState({ currencyAmount: event.target.value });
+    this.setState({ amount: event.target.value });
+    this.getRatesData(event.target.value);
   }
 
   changeSecondCurrency = (event)=>{
     this.setState({ secondCurrency: event.target.value });
+    this.getRatesData(event.target.value);
   }
+// need to resolve issue here, cannot add second currecny for the api fetch
+  getRatesData = (amount,base,secondCurrency) => {
+    this.setState({ loading: true });
+    fetch(`https://altexchangerateapi.herokuapp.com/latest?amount=${amount}from=${base}&to=${secondCurrency}`)
+      .then(Status)
+      .then(json)
+      .then(data => {
+        console.log(data)
+        if (data.error) {
+          throw new Error(data.error);
+        }
 
-  render() {
+        const rates = Object.keys(data.rates)
+          .filter(acronym => acronym !== base||secondCurrency)
+          .map(acronym => ({
+            acronym,
+            rate: data.rates[acronym],
+            name: currencies[acronym].name,
+            symbol: currencies[acronym].symbol,
+          }))
+
+        this.setState({ rates, loading: false });
+      })
+      .catch(error => console.error(error.message));
+  }
+  render(getRatesData) {
     const { base,secondCurrency} = this.state;
-    console.log(base,secondCurrency)
     return (
       <>
       <div className="container">
@@ -40,12 +69,12 @@ class CurrencyConverter extends React.Component  {
               type='text'
               placeholder='Enter currency amount'
               onChange={this.changeCurrencyAmount}            
-            />
+              />
                 <select value={base} onChange={this.changeBase} className="form-control form-control-lg " >
                   {Object.keys(currencies).map(currencyAcronym => <option key={currencyAcronym} value={currencyAcronym}>{currencyAcronym}</option>)}
                 </select>
                 <div class="text-center">
-                    <img className='arrow' src={require('./images/arrow.svg').default} alt='arrow' />
+                    <img className='arrow' src={require('./images/arrow.svg').default} alt='convert-arrow' />
                     </div>
                 <select value={secondCurrency} onChange={this.changeSecondCurrency} className="form-control form-control-lg " >
                 {Object.keys(currencies).map(currencyAcronym => <option key={currencyAcronym} value={currencyAcronym}>{currencyAcronym}</option>)}
