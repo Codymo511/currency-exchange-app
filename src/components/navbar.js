f7ac817a9d6584de118896894e1fb724bfca91d2
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2022-05-14 17:00:51 -0400

    changed header color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../styles/navstyle.css'
 import { Navbar, Nav,NavDropdown,Container} from 'react-bootstrap';
 
 class navbar extends React.Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Currency Exchange</Navbar.Brand>
+      <Navbar expand="lg">
+      <Container >
+        <Navbar.Brand href="#home" className='text-white'>Currency Exchange</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Portfolio</Nav.Link>
+          <Nav className="me-auto text-white">
+            <Nav.Link className="pl-5"href="#home">Portfolio</Nav.Link>
             <Nav.Link href="#link">Github</Nav.Link>
           </Nav>
         </Navbar.Collapse>
