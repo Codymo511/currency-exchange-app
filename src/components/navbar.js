1b1203a0a668d305c049c106ab99d79feb0993b4
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2022-05-26 08:46:21 -0400

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ class navbar extends React.Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto text-white">
             <Nav.Link className="pl-5 text-white"href="https://pedantic-mahavira-86c479.netlify.app/">Portfolio</Nav.Link>
-            <Nav.Link className="text-white"href="#link">Github</Nav.Link>
+            <Nav.Link className="text-white"href="https://github.com/Codymo511">Github</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
